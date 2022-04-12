7bffac83af24c5e29652a60c8d910c868b642853
reona kasuya <reon.kasuya@gmail.com>
2022-04-12 23:18:27 +0900

    console checked

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -10,6 +10,7 @@ class Airline < ApplicationRecord
   end
 
   # simple way to round the review scores and change into a float
+  # it gets the :score from all the reviews made available through the has_many reviews
   def avg_score
     reviews.average(:score).round(2).to_f
   end
