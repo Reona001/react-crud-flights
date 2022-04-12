036040f555b07951ec7eaddcef0cf669ea4e7d0a
reona kasuya <reon.kasuya@gmail.com>
2022-04-12 22:51:23 +0900

    slugify method added

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,12 @@
 class Airline < ApplicationRecord
+  has_many :reviews
+  # This method takes the name of the airline and slugifies it.
+  # Parameterize changes string to a url safe format like "united-airlines"
+  # Slugify is called before we save to the database so we use a before create callback
+  before_create :slugify
+
+
+  def slugify
+    self.slug = name.parameterize
+  end
 end
