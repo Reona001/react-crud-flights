cd83eeabd40aa3d834b414c720afa0251e84aee6
reona kasuya <reon.kasuya@gmail.com>
2022-04-13 23:56:20 +0900

    create and update added

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,6 +1,45 @@
 mudule Api
   module v1
     class AirlinesController < ApplicationController
+
+      def index
+        # This renders all the airline companies using the serializer
+        # Calls all airlines and passed it into the AirlineSerializer
+        airlines = Airline.all
+        render json: AirlineSerializer.new(airlines).serialized_json
+      end
+
+      def show
+        airline = Airline.find_by(slug: params[:slug])
+        render json: AirlineSerializer.new(airline).serialized_json
+      end
+
+      def create
+        airline = Airline.new(airline_params)
+
+        if airline.save
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        airline = Airline.find_by(slug: params[:slug])
+
+        if airline.update(airline_params)
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json: { error: airline.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def airline_params
+        params.require(:airline).permit(:name,:image_url)
+      end
+
     end
   end
 end
