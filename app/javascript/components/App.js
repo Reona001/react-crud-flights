0c8b9998b6bb1c188f076d3b831a77b06fc673a7
reona kasuya <reon.kasuya@gmail.com>
2022-04-18 10:31:54 +0900

    Airline and Airlines Components created

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Airlines from './Airlines/Airlines'
+import Airline from './Airline/Airline'
 
 const App = () => {
-  return (<div><h1>Hello World</h1></div>)
+  return (
+  <Switch>
+    <Route exact path="/" component={Airlines}/>
+    <Route exact path="/airlines/:slug" component={Airline} />
+  </Switch>)
+  // To show individual airlines like a traditional show page
+  // - on react you can pass a param on the url
+  // To keep things consistent we will pass our slugified name
+  // return (<div><h1>Hello World</h1></div>)
 }
 
 export default App
